fc5381a5324488c8fc851f5caa0689939b422139
林康 <linkang@innobuddy.com>
2020-04-22 22:35:46 +0800

    chore: bump version 1.0.15

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,18 +1,28 @@
 <template>
   <div ref="container" class="search">
     <div class="input-wrap">
-      <input ref="input" type="search" placeholder="搜索 API或文档" v-model="keyword" />
+      <input
+        ref="input"
+        type="search"
+        placeholder="搜索 API或文档"
+        v-model="keyword"
+      />
       <clear-button :show="!!keyword" @click="clear"></clear-button>
     </div>
-    <div class="results-panel" :class="{show:keyword}">
+    <div class="results-panel" :class="{ show: keyword }">
       <p class="empty" v-if="results.length === 0">无结果</p>
-      <div class="matching-post" v-for="(item,index) in results" :key="index" @click="clear">
+      <div
+        v-for="(item, index) in results"
+        :class="'matching-post ' + item.className"
+        :key="index"
+        @click="clear"
+      >
         <router-link :to="item.link">
           <h2>
-            <small v-if="item.type">{{item.type}}</small>
-            <span>{{item.title}}</span>
+            <small v-if="item.type">{{ item.type }}</small>
+            <span>{{ item.title }}</span>
           </h2>
-          <p v-if="item.text">{{item.text}}</p>
+          <p v-if="item.text">{{ item.text }}</p>
         </router-link>
       </div>
     </div>
@@ -24,11 +34,11 @@ import escapeRegexp from 'lodash.escaperegexp';
 import ClearButton from './ClearButton.vue';
 
 const makeRegexp = keyword => {
-  const matches = keyword.match(/\w+|[\u4e00-\u9fa5]|[^\w\s\u4e00-\u9fa5]+/g);
+  const matches = keyword.match(/\S/g);
   if (!matches) {
     return null;
   }
-  const pattern = matches.map(escapeRegexp).join('.{0,100}');
+  const pattern = matches.map(escapeRegexp).join('.{0,100}?');
   return new RegExp(pattern, 'i');
 };
 
@@ -92,21 +102,26 @@ export default {
       if (regexp) {
         const results = [];
         for (const controller of (this.spec && this.spec.modules) || []) {
-          if (regexp.test(controller.title)) {
+          const matchController = regexp.exec(controller.title);
+          if (matchController) {
             results.push({
-              order: 2000 + controller.name.length,
+              className: 'api controller',
+              order:
+                matchController[0].length * 10000 +
+                matchController.input.length,
               title: controller.title,
               text: '控制器',
               link: `/api/controller/${controller.name}`,
             });
           }
           for (const route of controller.actions || []) {
-            if (
-              regexp.test(route.title) ||
-              regexp.test(`${route.route.method} ${route.route.path}`)
-            ) {
+            const matchAction =
+              regexp.exec(route.title) ||
+              regexp.exec(`${route.route.method} ${route.route.path}`);
+            if (matchAction) {
               results.push({
-                order: 3000 + route.route.path.length,
+                className: 'api action',
+                order: matchAction[0].length * 10000 + matchAction.input.length,
                 title: `${route.title}（${controller.title}）`,
                 text: `接口：${route.route.method} ${route.route.path}`,
                 link: `/api/controller/${controller.name}#${route.name}`,
@@ -114,34 +129,32 @@ export default {
             }
           }
         }
+        /**
+         * @param {RegExp} regexp
+         * @param {string} title
+         * @param {string[]} paragraphs
+         */
         const match = (regexp, title, paragraphs) => {
-          if (
-            regexp.test(title) ||
-            paragraphs.some(content => regexp.test(content))
-          ) {
-            let index = 0;
-            const matchContent = paragraphs.find(content =>
-              regexp.test(content),
-            );
-            const content = matchContent || paragraphs[0] || '';
-            if (matchContent) {
-              const match = matchContent.match(regexp);
-              index = Math.max(
-                0,
-                Math.min(match.index - 20, matchContent.length - limit + 20),
-              );
+          let matchContent = regexp.exec(title);
+          for (const content of paragraphs) {
+            if (!matchContent) {
+              matchContent = regexp.exec(content);
             }
-            let prefix = index > 0 ? '...' : '';
-            let suffix = index + limit < content.length ? '...' : '';
+          }
+          if (matchContent) {
+            const content = matchContent.input;
+            const index = Math.max(
+              0,
+              Math.min(matchContent.index - 20, content.length - limit + 20),
+            );
+            let prefix = index > 0 ? '…' : '';
+            let suffix = index + limit < content.length ? '…' : '';
             return {
-              result: true,
+              order: matchContent[0].length * 10000 * matchContent.input.length,
               preview: prefix + content.slice(index, index + limit) + suffix,
             };
           }
-          return {
-            result: false,
-            preview: '',
-          };
+          return null;
         };
         for (const group of (this.spec && this.spec.docs) || []) {
           for (const item of group.list) {
@@ -151,9 +164,10 @@ export default {
                 section.title,
                 section.content || [],
               );
-              if (sectionMatch.result) {
+              if (sectionMatch) {
                 results.push({
-                  order: 1000,
+                  className: 'document section',
+                  order: sectionMatch.order,
                   title: item.title + ' • ' + section.title,
                   text: sectionMatch.preview || section.title,
                   link: section.link,
@@ -161,8 +175,10 @@ export default {
               }
             }
             const itemMatch = match(regexp, item.title, item.content || []);
-            if (itemMatch.result) {
+            if (itemMatch) {
               results.push({
+                className: 'document toplevel',
+                order: itemMatch.order,
                 title: item.title,
                 text: itemMatch.preview || item.title,
                 link: item.link,
